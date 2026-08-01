1a46695807d0c96c20953f3d37b4b57edf4cf346
Legakwa Selemogo <mrselemogo777@gmail.com>
2026-08-01 12:13:40 +0200

    Add adult blocklist Layer 0

--- services/url_service.py
+++ services/url_service.py
@@ -135,7 +135,7 @@ BLACKLISTED_DOMAINS = {
     "snipr.com", "snipurl.com", "sp2.ro", "tinyarrow.com",
     "twitclicks.com", "twitthis.com", "u.bb", "ul.to",
     "url.ie", "url4e.com", "urlborg.com", "whurl.com",
-    "xrl.us", "yep.it", "zip.net","pornhub.com","www.pornhub.com","https://pornhub.com","xvideoes.com","https://xvideos.com","www.xvideos.com","https://xnxx.com","xnxx.com","www.xnxx.com"
+    "xrl.us", "yep.it", "zip.net","pornhub.com","www.pornhub.com","https://www.pornhub.com","https://pornhub.com","xvideos.com","https://www.xvideos.com","https://xvideos.com","www.xvideos.com","https://xnxx.com","https://www.xnxx.com","xnxx.com","www.xnxx.com"
 }
 
 
